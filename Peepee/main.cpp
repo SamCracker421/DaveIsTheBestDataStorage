a81eab1623a0a8d4b4a811014449af79338a2321
Sam Blodgett <Pleasedontspamme421@gmail.com>
2022-04-08 11:46:10 -0400

    Update main.cpp

--- Peepee/main.cpp
+++ Peepee/main.cpp
@@ -27,7 +27,14 @@ using namespace mssm;
 bool onedirection(Vec2d point, vector<Vec2d>& points){
 double line=point.x;
 int intersections=0;
+int hitvertice=0;
 if(points.size()>3){
+double lowestyval=points[0].y;
+for(int x=0;x<points.size();x++){
+    if(lowestyval>points[x].y){
+        lowestyval=points[x].y;
+    }
+}
 for(int x=0;x<points.size()-1;x++){
     if(points[x].x-points[x+1].x==0){
     if(point.x==points[x].x){
@@ -44,22 +51,25 @@ for(int x=0;x<points.size()-1;x++){
     }
     }
     else{
-        if(point.x==points[x].x && points[x].y>point.y){
-            intersections+=2;
-        }
-        else if(point.x==points[x].x && points[x].y>point.y){
+        if(point.x==points[x].x && points[x].y==point.y){
             return true;
         }
         else{
-        int b=points[x].y-(points[x].y-points[x+1].y)/(points[x].x-points[x+1].x)*points[x].x;
+        int b=points[x].y-(((points[x].y-points[x+1].y)/(points[x].x-points[x+1].x))*points[x].x);
         if(points[x].x>points[x+1].x){
-        if(((points[x].y-points[x+1].y)/(points[x].x-points[x+1].x)*line+b>=point.y) && (point.x<points[x].x) && (point.x>points[x+1].x)){
+        if(((points[x].y-points[x+1].y)/(points[x].x-points[x+1].x)*point.x+b>point.y) && (point.x<points[x].x) && (point.x>=points[x+1].x)){
         intersections+=1;
+        }
+        else if(((points[x].y-points[x+1].y)/(points[x].x-points[x+1].x)*point.x+b==point.y) && (point.x<=points[x].x) && (point.x>=points[x+1].x)){
+            return true;
         }
         }
         else{
-            if(((points[x].y-points[x+1].y)/(points[x].x-points[x+1].x)*line+b>=point.y) && (point.x>points[x].x) && (point.x<points[x+1].x)){
+            if(((points[x].y-points[x+1].y)/(points[x].x-points[x+1].x)*line+b>point.y) && (point.x>=points[x].x) && (point.x<points[x+1].x)){
             intersections+=1;
+            }
+            else if(((points[x].y-points[x+1].y)/(points[x].x-points[x+1].x)*point.x+b==point.y) && (point.x>=points[x].x) && (point.x<=points[x+1].x)){
+                return true;
             }
         }
         }
@@ -80,11 +90,15 @@ else{
 int main()
 {
     Graphics g("CDthesesnutes", 1920, 1024);
+    srand(time(NULL));
     vector<Vec2d> polygons;
-
+    vector<Vec2d> points;
+    for(int z=0; z<50000;z++){
+        Vec2d potato={rand()%g.width(),rand()%g.height()};
+        points.push_back(potato);
+    }
     while (g.draw()) {
-        Vec2d point=g.mousePos();
-        g.text({0,20},10,to_string(onedirection(point,polygons)));
+        g.text({0,20},10,"x: " + to_string(g.mousePos().x) + " y: " + to_string(g.mousePos().y));
         for (const Event& e : g.events()) {
             switch (e.evtType) {
             case EvtType::MousePress:
@@ -98,9 +112,16 @@ int main()
                 break;
             }
         }
+        for(int x=0;x<points.size();x++){
+            if(onedirection(points[x],polygons)){
+                   g.ellipse(points[x],2,2,GREEN,GREEN);
+            }
+            else{
+                   g.ellipse(points[x],2,2,RED,RED);
+            }
+        }
         if(polygons.size()>0){
         g.polygon(polygons,WHITE);
-        g.ellipse(point,5,5,BLUE,BLUE);
         }
         }
 
